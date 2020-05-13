71f7ada37719f7efdc07a7068c12167750ecbe0b
sivasivagithub <48113015+sivasivagithub@users.noreply.github.com>
2020-05-14 01:10:45 +0530

    Add files via upload

---
+++ secondprg.py
@@ -0,0 +1,19 @@
+
+import tensorflow as tf
+import datetime
+
+# Using the MNIST dataset as the example, normalize the data and write a function that creates a simple Keras model for classifying the images into 10 classes.
+
+mnist = tf.keras.datasets.mnist
+
+(x_train, y_train),(x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+def create_model():
+  return tf.keras.models.Sequential([
+    tf.keras.layers.Flatten(input_shape=(28, 28)),
+    tf.keras.layers.Dense(512, activation='relu'),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dense(10, activation='softmax')
+  ])
+print("End")
